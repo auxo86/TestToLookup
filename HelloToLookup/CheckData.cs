77e9224e689929df09d05764f5fa91d546d73b99
U-MISNTPR\auxo <auxo@HPVDI-0051.ndmctsgh.edu.tw>
2020-05-06 10:32:23 +0800

    2nd commit

---
+++ HelloToLookup/CheckData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloToLookup
+{
+    public class CheckAllData
+    {
+        public string PatientId { get; set; }
+        public string CheckId { get; set; }
+        public double? Maximun { get; set; }
+        public double? Minimun { get; set; }
+        public DateTime RecDate { get; set; }
+        public double? TestNum { get; set; }
+
+    }
+
+    public class TestData
+    {
+        public DateTime RecDate { get; set; }
+        public double? TestNum { get; set; }
+    }
+
+    public class CheckResult
+    {
+        public string PatientId { get; set; }
+        public string CheckId { get; set; }
+        public double? Maximun { get; set; }
+        public double? Minimun { get; set; }
+
+        public IEnumerable<TestData> ChkData { get; set; }
+    }
+
+    
+}
---
+++ HelloToLookup/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloToLookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lookup = getDatas().ToLookup(a => new CheckResult
+            {
+                PatientId = a.PatientId,
+                CheckId = a.CheckId,
+                Maximun = a.Maximun,
+                Minimun = a.Minimun
+            });
+            //var lookup = getDatas().ToLookup(a => new
+            //{
+            //    a.PatientId,
+            //    a.CheckId,
+            //    a.Maximun,
+            //    a.Minimun
+            //});
+            var groupCount = 0;
+            var itemCount = 0;
+            foreach (var group in lookup)
+            {
+                groupCount++;
+                foreach (var item in group)
+                {
+                    itemCount++;
+                }
+            }
+
+            Console.WriteLine(groupCount);
+            Console.WriteLine(itemCount);
+            Console.ReadKey();
+        }
+
+        static List<CheckAllData> getDatas()
+        {
+            return new List<CheckAllData>()
+            {
+                new CheckAllData { PatientId = "12345", CheckId = "1", Maximun = 4.5, Minimun = 3,RecDate = new DateTime(2020, 1, 1), TestNum = 5.3},
+                new CheckAllData { PatientId = "12345", CheckId = "1", Maximun = 4.5, Minimun = 3,RecDate = new DateTime(2020, 2, 1), TestNum = 3.3},
+                new CheckAllData { PatientId = "12345", CheckId = "1", Maximun = 4.5, Minimun = 3,RecDate = new DateTime(2020, 3, 1), TestNum = 4.3},
+                new CheckAllData { PatientId = "12345", CheckId = "1", Maximun = 4.5, Minimun = 3,RecDate = new DateTime(2020, 4, 1), TestNum = 3.9},
+                new CheckAllData { PatientId = "12345", CheckId = "1", Maximun = 4.5, Minimun = 3,RecDate = new DateTime(2020, 5, 1), TestNum = 7.0},
+                new CheckAllData { PatientId = "12345", CheckId = "2", Maximun = 8.5, Minimun = 11,RecDate = new DateTime(2020, 1, 1), TestNum = 11},
+                new CheckAllData { PatientId = "12345", CheckId = "2", Maximun = 8.5, Minimun = 11,RecDate = new DateTime(2020, 2, 1), TestNum = 12},
+                new CheckAllData { PatientId = "12345", CheckId = "2", Maximun = 8.5, Minimun = 11,RecDate = new DateTime(2020, 3, 1), TestNum = 15},
+                new CheckAllData { PatientId = "12345", CheckId = "2", Maximun = 8.5, Minimun = 11,RecDate = new DateTime(2020, 4, 1), TestNum = 13},
+                new CheckAllData { PatientId = "12345", CheckId = "2", Maximun = 8.5, Minimun = 11,RecDate = new DateTime(2020, 5, 1), TestNum = 8},
+            };
+        }
+    }
+}
